462af08ec3f38c805279b808d427354134c71286
Effiong Uyo <e.uyo@alustudent.com>
2026-09-14 11:23:03 +0200

    Task 7: Render TeamDashboard component

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import TeamDashboard from "./TeamDashboard";
 function App() {
   return (
     <div>
-      <h1>Team Dashboard</h1>
-      <p>Welcome to our group application.</p>
+      <TeamDashboard />
     </div>
   );
 }
